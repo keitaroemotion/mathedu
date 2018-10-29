b5b508bfea7d43f36a5051b8459062442dfe475e
Kei Sugano <tobasojyo@gmail.com>
2018-10-29 13:04:54 +0900

    great improvement! now switching done

--- Assets/Shape.cs
+++ Assets/Shape.cs
@@ -7,8 +7,17 @@ public class Shape : MonoBehaviour {
 
     private int   objectIndex;
     private float delta;
+    private float VELOCITY = 0.1f;
 
     private List<string> objects = new List<string>{"Cylinder", "Cube", "Capsule"};
+
+    private Vector3 POSITION_2   = new Vector3(-1.4f, 0.0f, -5.7f);
+    private Vector3 POSITION_1   = new Vector3(0.52f, 0.79f, -7.7f);
+    private Vector3 POSITION_3   = new Vector3(4.0f, 0.0f, -5.7f);
+
+    private List<Vector3> GetPositions(){
+        return new List<Vector3>{POSITION_1, POSITION_2, POSITION_3};
+    }
 
     ObjectController GetObjectController(){
         return GameObject.Find("Object").GetComponent<ObjectController>();
@@ -56,9 +65,46 @@ public class Shape : MonoBehaviour {
         Debug.Log(string.Format("index: {0} cnt: {1} delta: {2}", objectIndex, objects.Count, delta));
     }
 
+    GameObject GetTargetObject(){
+        return GetTargetObjectWithIndex(objectIndex);
+    }
+
+    GameObject GetTargetObjectWithIndex(int i){
+        return GameObject.Find(objects[i]);
+    }
+
+    List<Vector3> SubList(List<Vector3> v, int start, int end){
+	var newList = new List<Vector3>();    
+        for(int i = start; i <= end;  i++)  {
+           newList.Add(v[i]);	
+	}
+	return newList;
+    }
+     
+    List<Vector3> SetPositions(int i, List<Vector3> v){
+	var N      = v.Count;    
+	var former = SubList(v, 0,     N - (i + 1));
+	var latter = SubList(v, N - i, N - 1);
+	latter.AddRange(former);
+	return latter;
+    }
+
     void Update () {
+
 	SwitchObjects();
         ShowLog();
-        GetObjectController().Rotate(GameObject.Find(objects[objectIndex]));
+        var targetObject = GetTargetObject();
+
+	var positions = SetPositions(objectIndex, GetPositions());
+
+	// targetObject.transform.position = GetPositions()[objectIndex];
+	for(var i = 0; i < objects.Count; i++){
+            var obj = GetTargetObjectWithIndex(i);
+	    //obj.transform.position = positions[i];
+	    obj.transform.position = Vector3.MoveTowards(obj.transform.position, positions[i], VELOCITY);
+	}
+
+        GetObjectController().Rotate(targetObject);
+	//targetObject.transform.position = Vector3.MoveTowards(targetObject.transform.position, targetPosition, VELOCITY);
     }
 }
